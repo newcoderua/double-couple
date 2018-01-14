939e9b5630dd63333f668fee63279adfb2bbb4c2
vladyslav <tradestadnyk@gmail.com>
2018-01-14 18:45:08 -0500

    start working on if user like or dislike the card, using panResponderRelease

--- src/Deck.js
+++ src/Deck.js
@@ -22,16 +22,24 @@ class Deck extends Component {
       onPanResponderMove: (evt, gestureState) => {
         position.setValue({ x: gestureState.dx, y:gestureState.dy });
       },
-      onPanResponderRelease: () => {}
+      onPanResponderRelease: (event, gesture) => {
+        this.resetPosition();
+      }
     });
 
     this.state = { panResponder, position };
   }
 
+  resetPosition() {
+    Animated.spring(this.state.position, {
+      toValue: { x : 0, y : 0 }
+    }).start();
+  }
+
   getCardStyle() {
     const { position } = this.state;
     const rotate = position.x.interpolate({
-      inputRange: [-SCREEN_WIDTH, 0, SCREEN_WIDTH],
+      inputRange: [-SCREEN_WIDTH * 1.5, 0, SCREEN_WIDTH * 1.5],
       outputRange: ['-120deg', '0deg', '120deg']
     });
 
